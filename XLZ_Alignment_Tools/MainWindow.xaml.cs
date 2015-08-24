582b40d7c0f2abb070f12446c973669826a88773
henry.hsu <hsu7117@gmail.com>
2015-08-24 13:58:36 +0800

    First Release

--- XLZ_Alignment_Tools/MainWindow.xaml.cs
+++ XLZ_Alignment_Tools/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using OfficeOpenXml;
 using System.IO;
 using SentenceSplitter;
 using Lionbridge.Sterling.Shared.XLZ;
+using System.Text.RegularExpressions;
 
 
 namespace XLZ_Alignment_Tools
@@ -30,6 +31,14 @@ namespace XLZ_Alignment_Tools
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LangaugeCode_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> Language = Properties.Resources.LanguageCode.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            SRC_Cbox.ItemsSource = Language;
+            TGT_Cbox.ItemsSource = Language;
         }
 
         private void ByFile_rBtn_Checked(object sender, RoutedEventArgs e)
@@ -53,64 +62,154 @@ namespace XLZ_Alignment_Tools
             TGT_File_tbx.Visibility = Visibility.Hidden;
         }
 
+        private void ByExcel_rBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            SRC_File_label.Content = "Excel File :";
+            TGT_File_label1.IsEnabled = false;
+            TGT_File_label1.Visibility = Visibility.Hidden;
+            TGT_File_tbx.IsEnabled = false;
+            TGT_File_tbx.Visibility = Visibility.Hidden;
+        }
 
         private void Generate_Click(object sender, RoutedEventArgs e)
         {
-            ResxFileParser.ResxFileParser SRC_RESX = new ResxFileParser.ResxFileParser();
-            SRC_RESX.FileParser(SRC_File_tbx.Text);
+            Regex Lang_pattern = new Regex(@"(.+?)\s->(?<Code>.+?-.+?)\b");
+            Match SRC_Code_Match = Lang_pattern.Match(SRC_Cbox.SelectedValue.ToString());
+            Match TGT_Code_Match = Lang_pattern.Match(TGT_Cbox.SelectedValue.ToString());
 
-            ResxFileParser.ResxFileParser TGT_RESX = new ResxFileParser.ResxFileParser();
-            TGT_RESX.FileParser(TGT_File_tbx.Text);
-
-            #region Generate Excel file to store resx file.
-            FileInfo Bilingual_Info = new FileInfo(Path.GetDirectoryName(SRC_File_tbx.Text) + @"\" + Path.GetFileNameWithoutExtension(SRC_File_tbx.Text) + ".xlsx");
-            #endregion
-            ExcelPackage Bilingual_Package = new ExcelPackage();
-            ExcelWorksheet Bilingual_WS = Bilingual_Package.Workbook.Worksheets.Add(Path.GetFileNameWithoutExtension(SRC_File_tbx.Text));
-            int current_position = 2;
-            foreach (var SRC_Value in SRC_RESX.DataName)
+            #region By FileList
+            if (ByMapplist_rBtn.IsChecked == true)
             {
-                foreach(var TGT_Value in TGT_RESX.DataName)
+                FileInfo List_Info = new FileInfo(SRC_File_tbx.Text);
+                ExcelPackage List_Package = new ExcelPackage(List_Info);
+                ExcelWorksheet List_WS = List_Package.Workbook.Worksheets[1];
+
+                for (int i = 2; i <= List_WS.Dimension.End.Row; i++)
                 {
-                    if(SRC_Value.Key==TGT_Value.Key)
+                    ResxFileParser.ResxFileParser SRC_RESX = new ResxFileParser.ResxFileParser();
+                    SRC_RESX.FileParser(List_WS.Cells[i, 1].Text);
+
+                    ResxFileParser.ResxFileParser TGT_RESX = new ResxFileParser.ResxFileParser();
+                    TGT_RESX.FileParser(List_WS.Cells[i, 2].Text);
+
+                    FileInfo Bilingual_Info = new FileInfo(Path.GetDirectoryName(List_WS.Cells[i, 1].Text) + @"\" + Path.GetFileNameWithoutExtension(List_WS.Cells[i, 1].Text) + ".xlsx");
+                    ExcelPackage Bilingual_Package = new ExcelPackage();
+                    ExcelWorksheet Bilingual_WS = Bilingual_Package.Workbook.Worksheets.Add(Path.GetFileNameWithoutExtension(List_WS.Cells[i, 1].Text));
+                    int current_position = 2;
+                    foreach (var SRC_Value in SRC_RESX.DataName)
                     {
-                        Bilingual_WS.Cells[current_position, 1].Value = SRC_Value.Key;
-                        Bilingual_WS.Cells[current_position, 2].Value = SRC_Value.Value;
-                        Bilingual_WS.Cells[current_position, 3].Value = TGT_Value.Value;
-                        current_position++;
+                        foreach (var TGT_Value in TGT_RESX.DataName)
+                        {
+                            if (SRC_Value.Key == TGT_Value.Key)
+                            {
+                                Bilingual_WS.Cells[current_position, 1].Value = SRC_Value.Key;
+                                Bilingual_WS.Cells[current_position, 2].Value = SRC_Value.Value;
+                                Bilingual_WS.Cells[current_position, 3].Value = TGT_Value.Value;
+                                current_position++;
+                            }
+                        }
+                    }
+                    XLZDocument ReviewXLZ = new XLZDocument(SRC_Code_Match.Groups["Code"].Value, "", TGT_Code_Match.Groups["Code"].Value);
+                    int processCount = 1;
+                    for (int z = 2; z <= Bilingual_WS.Dimension.End.Row; z++)
+                    {
+                        string[][] split_String = SentenceSplitter.SentenceSplitter.GetSplitedSegments(Bilingual_WS.Cells[z, 2].Text, Bilingual_WS.Cells[z, 3].Text, SRC_Code_Match.Groups["Code"].Value, TGT_Code_Match.Groups["Code"].Value);
+                        for (int j = 0; j < split_String[0].Count(); j++)
+                        {
+                            string currentSRC_String = split_String[0][j];
+                            string currentTGT_String = split_String[1][j];
+                            processCount++;
+                            var TU = ReviewXLZ.AddTransUnit(new XliffTransUnit((ReviewXLZ.TransUnits.Count + 1).ToString(), true), true);
+                            TU.SourceRaw = System.Security.SecurityElement.Escape(currentSRC_String);
+                            TU.TranslationRaw = System.Security.SecurityElement.Escape(currentTGT_String);
+                            ReviewXLZ.AppendFormattingAfterTransUnit(TU, "\r\n");
+                        }
+                    }
+                    Bilingual_Package.SaveAs(Bilingual_Info);
+                    ReviewXLZ.Save(Path.GetDirectoryName(List_WS.Cells[i, 1].Text) + @"\" + Path.GetFileNameWithoutExtension(List_WS.Cells[i, 1].Text) + ".xlz");
+                }
+            }
+            #endregion
+            #region By File
+            if (ByFile_rBtn.IsChecked == true)
+            {
+                ResxFileParser.ResxFileParser SRC_RESX = new ResxFileParser.ResxFileParser();
+                SRC_RESX.FileParser(SRC_File_tbx.Text);
+
+                ResxFileParser.ResxFileParser TGT_RESX = new ResxFileParser.ResxFileParser();
+                TGT_RESX.FileParser(TGT_File_tbx.Text);
+
+
+                #region Generate Excel file to store resx file.
+                FileInfo Bilingual_Info = new FileInfo(Path.GetDirectoryName(SRC_File_tbx.Text) + @"\" + Path.GetFileNameWithoutExtension(SRC_File_tbx.Text) + ".xlsx");
+                #endregion
+                ExcelPackage Bilingual_Package = new ExcelPackage();
+                ExcelWorksheet Bilingual_WS = Bilingual_Package.Workbook.Worksheets.Add(Path.GetFileNameWithoutExtension(SRC_File_tbx.Text));
+                int current_position = 2;
+                foreach (var SRC_Value in SRC_RESX.DataName)
+                {
+                    foreach (var TGT_Value in TGT_RESX.DataName)
+                    {
+                        if (SRC_Value.Key == TGT_Value.Key)
+                        {
+                            Bilingual_WS.Cells[current_position, 1].Value = SRC_Value.Key;
+                            Bilingual_WS.Cells[current_position, 2].Value = SRC_Value.Value;
+                            Bilingual_WS.Cells[current_position, 3].Value = TGT_Value.Value;
+                            current_position++;
+                        }
                     }
                 }
-            }
-            XLZDocument ReviewXLZ = new XLZDocument("en-us", "", "zh-cn");
-            int processCount = 1;
-            for (int i = 2; i <= Bilingual_WS.Dimension.End.Row; i++)
-            {
-                string[][] split_String = SentenceSplitter.SentenceSplitter.GetSplitedSegments(Bilingual_WS.Cells[i, 2].Text, Bilingual_WS.Cells[i, 3].Text, "en-US", "zh_CN");
-                for (int j = 0; j < split_String[0].Count() ; j++)
+
+                XLZDocument ReviewXLZ = new XLZDocument(SRC_Code_Match.Groups["Code"].Value, "", TGT_Code_Match.Groups["Code"].Value);
+                int processCount = 1;
+                for (int i = 2; i <= Bilingual_WS.Dimension.End.Row; i++)
                 {
-                    string currentSRC_String = split_String[0][j];
-                    string currentTGT_String = split_String[1][j];
-                    processCount++;
-                    var TU = ReviewXLZ.AddTransUnit(new XliffTransUnit((ReviewXLZ.TransUnits.Count + 1).ToString(), true), true);
-                    TU.SourceRaw = System.Security.SecurityElement.Escape(currentSRC_String);
-                    TU.TranslationRaw = System.Security.SecurityElement.Escape(currentTGT_String);
-                    ReviewXLZ.AppendFormattingAfterTransUnit(TU, "\r\n");
+                    string[][] split_String = SentenceSplitter.SentenceSplitter.GetSplitedSegments(Bilingual_WS.Cells[i, 2].Text, Bilingual_WS.Cells[i, 3].Text, "en-US", "zh_CN");
+                    for (int j = 0; j < split_String[0].Count(); j++)
+                    {
+                        string currentSRC_String = split_String[0][j];
+                        string currentTGT_String = split_String[1][j];
+                        processCount++;
+                        var TU = ReviewXLZ.AddTransUnit(new XliffTransUnit((ReviewXLZ.TransUnits.Count + 1).ToString(), true), true);
+                        TU.SourceRaw = System.Security.SecurityElement.Escape(currentSRC_String);
+                        TU.TranslationRaw = System.Security.SecurityElement.Escape(currentTGT_String);
+                        ReviewXLZ.AppendFormattingAfterTransUnit(TU, "\r\n");
+                    }
+                }
+                Bilingual_Package.SaveAs(Bilingual_Info);
+                ReviewXLZ.Save(Path.GetDirectoryName(SRC_File_tbx.Text) + @"\" + Path.GetFileNameWithoutExtension(SRC_File_tbx.Text) + ".xlz");
+            }
+            #endregion
+            #region Excel
+            if (ByExcel_rBtn.IsChecked == true)
+            {
+                FileInfo Bilingual_Info = new FileInfo(SRC_File_tbx.Text);
+                ExcelPackage Bilingual_Package = new ExcelPackage(Bilingual_Info);
+
+                XLZDocument ReviewXLZ = new XLZDocument(SRC_Code_Match.Groups["Code"].Value, "", TGT_Code_Match.Groups["Code"].Value);
+                int processCount = 1;
+                for (int z = 1; z <= Bilingual_Package.Workbook.Worksheets.Count; z++)
+                {
+                    ExcelWorksheet Bilingual_WS = Bilingual_Package.Workbook.Worksheets[z];
+                    for (int i = 2; i <= Bilingual_WS.Dimension.End.Row; i++)
+                    {
+                        string[][] split_String = SentenceSplitter.SentenceSplitter.GetSplitedSegments(Bilingual_WS.Cells[i, 1].Text, Bilingual_WS.Cells[i, 2].Text, "en-US", "zh_CN");
+                        for (int j = 0; j < split_String[0].Count(); j++)
+                        {
+                            string currentSRC_String = split_String[0][j];
+                            string currentTGT_String = split_String[1][j];
+                            processCount++;
+                            var TU = ReviewXLZ.AddTransUnit(new XliffTransUnit((ReviewXLZ.TransUnits.Count + 1).ToString(), true), true);
+                            TU.SourceRaw = System.Security.SecurityElement.Escape(currentSRC_String);
+                            TU.TranslationRaw = System.Security.SecurityElement.Escape(currentTGT_String);
+                            ReviewXLZ.AppendFormattingAfterTransUnit(TU, "\r\n");
+                        }
+                    }
+                    ReviewXLZ.Save(Path.GetDirectoryName(SRC_File_tbx.Text) + @"\" + Path.GetFileNameWithoutExtension(SRC_File_tbx.Text) + "_Sheet"+z.ToString()+".xlz");
                 }
             }
-            Bilingual_Package.SaveAs(Bilingual_Info);
-            ReviewXLZ.Save(Path.GetDirectoryName(SRC_File_tbx.Text) + @"\" + Path.GetFileNameWithoutExtension(SRC_File_tbx.Text) + ".xlz");
-
-            MessageBox.Show("Done");
-            //MessageBox.Show(SRC_RESX.DataName.Count.ToString());
-            //MessageBox.Show(TGT_RESX.DataName.Count.ToString());
-        }
-
-        private void LangaugeCode_Loaded(object sender, RoutedEventArgs e)
-        {
-            List<string> Language = Properties.Resources.LanguageCode.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList();
-
-            SRC_Cbox.ItemsSource = Language;
-            TGT_Cbox.ItemsSource = Language;
+            #endregion
+        MessageBox.Show("Done");
         }
     }
 }
